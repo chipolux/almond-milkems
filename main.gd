f07598e66357e56f2634ff8862dbb179433cd2c2
NaKyle Wright <nakyle.wright@gmail.com>
2018-08-22 17:48:37 -0500

    Add strike/bad review and score/happy customer display.

--- main.gd
+++ main.gd
@@ -3,19 +3,29 @@ extends Node
 onready var delivery_area = get_node("milk_delivery")
 onready var player = get_node("entities/player")
 
-var milk_count = 7
-var max_milk = 8
+var milk_count = 0
+var strike_count = 0
+var score = 0
+var max_milks
+var max_strikes
 
 func _ready():
+	max_milks = get_node("milks").get_child_count()
+	max_strikes = get_node("strikes").get_child_count()
 	player.connect("place_milk", self, "place_milk")
 
 func _process(delta):
-	for i in range(max_milk):
-		i += 1
-		if milk_count >= i:
+	for i in range(max_milks):
+		if milk_count > i:
 			get_node("milks/milk%s" % i).show()
 		else:
 			get_node("milks/milk%s" % i).hide()
+	for i in range(max_strikes):
+		if strike_count > i:
+			get_node("strikes/strike%s" % i).modulate = Color(1, 1, 1, 1)
+		else:
+			get_node("strikes/strike%s" % i).modulate = Color(1, 1, 1, 0.2)
+	get_node("score").text = str(score)
 
 func player_in_delivery_area():
 	for body in delivery_area.get_overlapping_bodies():
@@ -24,6 +34,6 @@ func player_in_delivery_area():
 	return false
 
 func place_milk():
-	if milk_count < max_milk and player_in_delivery_area():
+	if milk_count < max_milks and player_in_delivery_area():
 		player.has_milk = false
 		milk_count += 1
